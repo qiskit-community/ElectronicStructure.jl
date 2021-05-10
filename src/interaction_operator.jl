48e4ebebcd1075ea8a5fd4fc8520cf7dca78b824
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-05-10 15:52:52 -0400

    modified:   src/interaction_operator.jl

--- src/interaction_operator.jl
+++ src/interaction_operator.jl
@@ -5,15 +5,15 @@
 ## Caution, this differs from OpenFermion by a factor of 1/2 in the two-body integral.
 ## In OpenFermion, they include the factor when constructing InteractionOperator
 function spin_orbital_from_spatial(one_body_integrals, two_body_integrals)
-    n_qubits = 2 * size(one_body_integrals)[1]
-    r1 = 1:2:n_qubits # odd indices
-    r2 = 2:2:n_qubits # even indices
+    n_side = 2 * size(one_body_integrals)[1]
+    r1 = 1:2:n_side # odd indices
+    r2 = 2:2:n_side # even indices
 
-    one_body_coefficients = similar(one_body_integrals, (n_qubits, n_qubits))
+    one_body_coefficients = similar(one_body_integrals, (n_side, n_side))
     one_body_coefficients[r1, r1] .= one_body_integrals
     one_body_coefficients[r2, r2] .= one_body_integrals
 
-    two_body_coefficients = similar(two_body_integrals, (n_qubits, n_qubits, n_qubits, n_qubits))
+    two_body_coefficients = similar(two_body_integrals, (n_side, n_side, n_side, n_side))
     for inds in ((r1, r2, r2, r1), (r2, r1, r1, r2), (r1, r1, r1, r1), (r2, r2, r2, r2))
         two_body_coefficients[inds...] .= two_body_integrals
     end
