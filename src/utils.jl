9ea93f70b9aa8430c5ec14e45727132d703a87a9
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-05-16 14:09:54 -0400

    refactor symmetry tests

--- src/utils.jl
+++ src/utils.jl
@@ -25,7 +25,7 @@ end
 Convert the rank-four tensor `two_body` representing two-body integrals from physicist
 index order to chemist index order: i,j,k,l -> i,k,l,j
 
-See `chem_to_phys`, `test_hijkl_symmetries`.
+See `chem_to_phys`, `test_two_body_symmetries`.
 """
 function phys_to_chem(two_body)
     @tullio two_body_out[i,k,l,j] := two_body[i,j,k,l]
@@ -38,7 +38,7 @@ end
 Convert the rank-four tensor `two_body` representing two-body integrals from chemist
 index order to physicist index order: i,j,k,l -> i,l,j,k
 
-See `phys_to_chem`, `test_hijkl_symmetries`.
+See `phys_to_chem`, `test_two_body_symmetries`.
 """
 function chem_to_phys(two_body)
     @tullio two_body_out[i,l,j,k] := two_body[i,j,k,l]
@@ -50,7 +50,7 @@ end
 ## 2. Make each tullio transform into a function that overwrites an input.
 ##    We don't want to allocate so many arrays.
 """
-    test_hijkl_symmetries(t; chemist=true)
+    test_two_body_symmetries(t; chemist=true)
 
 Test that rank-4 tensor `t` has the required symmetries for coefficents of the two-electron terms.
 If `chemist` is `true`, assume the input is in chemists' order, otherwise in physicists' order.
@@ -63,7 +63,7 @@ References: HJO (1.4.17), (1.4.38)
 
 See `phys_to_chem`, `chem_to_phys`.
 """
-function test_hijkl_symmetries(t_in::Array{T}; chemist=true) where T
+function test_two_body_symmetries(t_in::Array{T}; chemist=true) where T
     t = ZChop.zchop(t_in)
 
     if chemist  # chemists' ordering
@@ -98,3 +98,56 @@ function test_hijkl_symmetries(t_in::Array{T}; chemist=true) where T
     println(fail_count, "/", length(test_set), " tests failed.")
     return nothing
 end
+
+# function try_symmetries(two_body)
+#     (chem_tests, phys_tests) = symmetry_tests()
+#     t1 = similar(two_body)
+#     for tst in chem_tests
+#         tst(t1, two_body)
+#         println(t1 ≈ two_body)
+#     end
+# end
+
+"""
+    check_two_body_symmetries(two_body_tensor; chemist=true)
+
+Return `true` if `two_body_tensor` has the required symmetries. `chemist` is `true` for
+chemists' ordering of indices and `false` for physicists' ordering.
+"""
+function check_two_body_symmetries(two_body_tensor; chemist=true)
+    if chemist
+        tests = _chem_tests
+    else
+        tests = _phys_tests
+    end
+    t1 = similar(two_body_tensor)
+    for test! in tests
+        test!(t1, two_body_tensor)
+        if ! (t1 ≈ two_body_tensor)
+            return false
+        end
+    end
+    return true
+end
+
+## Tests for two-electron coefficient symmetries in chemists' order
+const _chem_tests = [
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[q, p, r, s]),  # (1.4.38) in HJO book
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[p, q, s, r]),  # (1.4.38)
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[q, p, s, r]),  # (1.4.38)
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[r, s, p, q]),  # (1.4.17)
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[r, s, q, p]),  # 1 and 4
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[s, r, p, q]),  # 2 and 4
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[s, r, q, p]),  # 3 and 4
+    ]
+
+## Tests for two-electron coefficient symmetries in physicists' order
+const _phys_tests = [
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[r, q, p, s]),
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[p, s, r, q]),
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[s, r, q, p]),
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[q, p, s, r]),
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[r, s, p, q]),
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[s, p, q, r]),
+        ((t1, t) ->  @tullio t1[p, q, r, s] = t[q, r, s, p])
+    ]
